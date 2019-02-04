19fdf42ff8499fd9bc6794699a10175c2fd6f621
Sam Migirditch <samigir@iu.edu>
2019-02-04 14:36:07 -0500

    Restructure and backup

---
+++ Examples/SimpleSearch/SimpleSearchMain.cpp
@@ -0,0 +1,50 @@
+// ***************************************************
+// A very simple example program for the TSearch class
+// ***************************************************
+
+#include "TSearch.h"
+
+// Parameters
+TVector<int> TempVec = {1,1};
+
+// A simple 2D inverted quadratic evaluation function
+
+double Evaluate(TVector<double> &v, RandomState &)
+{
+	double p1 = MapSearchParameter(v[1],-10,10), 
+           p2 = MapSearchParameter(v[2],-10,10);
+	return 200-(p1*p1+p2*p2);
+}
+
+
+// The main program
+
+int main (int argc, const char* argv[]) {
+  TSearch s(2);
+    
+  // Configure the search
+  s.SetRandomSeed(87632455);
+  s.SetEvaluationFunction(Evaluate);
+  s.SetSelectionMode(RANK_BASED);
+  s.SetReproductionMode(HILL_CLIMBING);
+  s.SetPopulationSize(5);//1000
+  s.SetMaxGenerations(20);
+  s.SetMutationVariance(0.1);
+  s.SetCrossoverProbability(0.5);
+  //SVM: attempting to group points.
+  //s.SetCrossoverPoints( TempVec );
+  s.SetCrossoverTemplate( TempVec );
+  s.SetCrossoverMode(TWO_POINT);
+  s.SetMaxExpectedOffspring(1.1);
+  s.SetElitistFraction(0.1);
+  s.SetSearchConstraint(1);
+  s.SetCheckpointInterval(5);
+    
+  // Run the search
+  s.ExecuteSearch();
+  
+  // Display the best individual found
+  cout << s.BestIndividual() << endl;
+
+  return 0;
+}
