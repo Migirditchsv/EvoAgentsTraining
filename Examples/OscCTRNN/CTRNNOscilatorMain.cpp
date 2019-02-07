e46d9e957ce99c7df0269ea2bb75adb16906e7e4
Sam Migirditch <samigir@iu.edu>
2019-02-07 14:15:37 -0500

    it lives!!!

--- Examples/OscCTRNN/CTRNNOscilatorMain.cpp
+++ Examples/OscCTRNN/CTRNNOscilatorMain.cpp
@@ -16,32 +16,46 @@
 
 // Control Pannel
 // Neuronal controls
-#define NEURON_GAIN   0.0
-#define RUN_DURATION  250
-#define STEP_SIZE     0.01
+const double NEURON_GAIN     = 0.0;
+const int    RUN_DURATION    = 250; // be reasonable now, make sure these divide nice
+const double STEP_SIZE       = 0.01;
+const int    STEP_NUM        = ceil(1+ (RUN_DURATION / STEP_SIZE) );
 // Search controls
-#define POPULATION_SIZE  50
-#define MAX_GENERATIONS  200 
-#define VARIANCE         0.1
+const double POPULATION_SIZE = 50;
+const int    MAX_GENERATIONS = 200; 
+const double VARIANCE        = 0.1;
 
-// Globals oh wow these are probably a bad idea
+// Global objects oh wow these are probably a bad idea
 CTRNN c(2);
 
 double Evaluate(TVector<double> &v, RandomState &)
 {
     // Warm up
-    double N1TimeSeries[RUN_DURATION];
-    double N2TimeSeries[RUN_DURATION];
+    double N1TimeSeries[STEP_NUM];
+    double N2TimeSeries[STEP_NUM];
     int    TimeIndex = 0;
+
+    // Map search Vectors
+    double w11 = MapSearchParameter( v[1], -10,10 );
+    double w12 = MapSearchParameter( v[2], -10,10 );
+    double w21 = MapSearchParameter( v[3], -10,10 );
+    double w22 = MapSearchParameter( v[4], -10,10 );
+    double b1  = MapSearchParameter( v[5], -10,10 );
+    double b2  = MapSearchParameter( v[6], -10,10 );
+    double g1  = MapSearchParameter( v[7], -10,10 );
+    double g2  = MapSearchParameter( v[8], -10,10 );
+    // Okay wow that's a very very large parameter space to search
+    // lets see how well this works. 
+
     // Set up the circuit
-        c.SetConnectionWeight(1, 1, v[1]);
-    c.SetConnectionWeight(1, 2, v[2]);
-    c.SetConnectionWeight(2, 1, v[3]);
-    c.SetConnectionWeight(2, 2, v[4]);
-    c.SetNeuronBias(1, v[5]); 
-    c.SetNeuronBias(2, v[6]);
-    c.SetNeuronGain(1, NEURON_GAIN );
-    c.SetNeuronGain(2, NEURON_GAIN );
+    c.SetConnectionWeight( 1, 1, w11 );
+    c.SetConnectionWeight( 1, 2, w12 );
+    c.SetConnectionWeight( 2, 1, w21 );
+    c.SetConnectionWeight( 2, 2, w22 );
+    c.SetNeuronBias( 1, b1 ); 
+    c.SetNeuronBias( 2, b2 );
+    c.SetNeuronGain( 1, g1 );
+    c.SetNeuronGain( 2, g2 );
 
 
     // Run the circuit
@@ -50,9 +64,11 @@ double Evaluate(TVector<double> &v, RandomState &)
     N2TimeSeries[0] = c.NeuronOutput(2);
     cout << "Simulating Network" << endl;
     for (double time = STEP_SIZE; time <= RUN_DURATION; time += STEP_SIZE) {
+        //cout<<"Attempting step to time:"<<time<<endl;
         TimeIndex += 1;
         c.EulerStep(STEP_SIZE);
-        if( fmod(time, 10*STEP_SIZE ) ==0 )
+        //cout<<"Step complete."<<endl;
+        if( fmod(time, 1000*STEP_SIZE ) ==0 )
             {
             cout<<time<<" "<<c.NeuronOutput(1)<<" "<<c.NeuronOutput(2)<< endl;
             }
